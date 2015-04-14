1b3aa75275867c2bb1ec9cc15c4cb90355a32664
CarlZhong2014 <CarlZhong2014@gmail.com>
2015-04-14 16:36:11 +0800

    add echo statement for admin

--- SeverBackup/ServerBackup.sh
+++ SeverBackup/ServerBackup.sh
@@ -31,7 +31,7 @@ NFSDIR="/tmp/nfs/nfsdir"
 #The log file of this shell script is saving in $LOGFILE.
 LOGFILE="/tmp/backup${DATE}.log"
 #The NFS Server ip and export directory
-NFSID="192.168.0.142:/tmp/nfsshare"
+NFSID="192.168.1.142:/tmp/nfsshare"
 #Function Description
 #camNFS implement to mount NFS and check the NFS which is mount in system.
 function camNFS()
@@ -45,8 +45,11 @@ function camNFS()
 #if you want to backup some thing that you can write down the backup command in this funciton.
 function BackupData()
 {
+    echo "    Beginning to backup /etc directory    "
     /bin/tar -zcvf $BKPDIR$PREFIX.etc.tar.gz /etc 1>/dev/null 2>>$LOGFILE
     LOG $? 2 "/etc"
+    echo "    /etc is backuped    "
+
     #/usr/local/mysql/bin/mysqldump -u root -p123456 --all-databases >$BKPDIR$PREFIX.mysqld.sql 1>/dev/null 2>>$LOGFILE
     #LOG $? 2
 }
@@ -56,7 +59,7 @@ function CleanUp()
 {
     if [ "$1" -eq 0 ]
     then
-        rm -f $LOGFILE
+        echo "Script is done, You can check $LOGFIL ."
     fi
     rm -fr ${BKPDIR}* 1>/dev/null 2>&1
 }
@@ -71,23 +74,23 @@ function LOG()
   then
       case $2 in
         1)
-            echo "[$(date)]: MountError: The remote directory is not mount in this system!!!" >>$LOGFILE
+            echo "[$(date)]: MountError: The remote directory is not mount in this system!!!" | tee -a $LOGFILE
             exit 1;;
         2)
-            echo "[$(date)]: BackupError: Couldn't backup $3 maybe permission dennis or disk is full" >>$LOGFILE
+            echo "[$(date)]: BackupError: Couldn't backup $3 maybe permission dennis or disk is full" | tee -a $LOGFILE
             CleanUp 1
             exit 2;;
         3)
-            echo "[$(date)]: TransistError: Backup data couldn't be copyed to $NFSDIR" >>$LOGFILE
+            echo "[$(date)]: TransistError: Backup data couldn't be copyed to $NFSDIR" | tee -a $LOGFILE
             /bin/umount $NFSDIR
             exit 3;;
         4)
-            echo "[$(date)]: $3 couldn't be created" >>$LOGFILE
-            echo "[$(date)]: Backup is unsuccessfully" >>$LOGFILE
+            echo "[$(date)]: $3 couldn't be created" | tee -a $LOGFILE
+            echo "[$(date)]: Backup is unsuccessfully" | tee -a $LOGFILE
             exit 4;;
 
         5)
-            echo "[$(date)]: UMountError: Couldn't umount the NFS directory" >>$LOGFILE
+            echo "[$(date)]: UMountError: Couldn't umount the NFS directory" | tee -a $LOGFILE
             exit 5;;
 
       esac
@@ -102,8 +105,10 @@ function LOG()
 #3. Create backup user whom uid is 505.
 #4. Clean the old data.
 echo "[$(date)]: Initialization" >>$LOGFILE
+echo "----Initialization----"
 if [ ! -d $BKPDIR ]
 then
+    echo "    make $BKPDIR directory    "
         mkdir $BKPDIR 1>/dev/null 2>>$LOGFILE
         LOG $? 4 $BKPDIR
         echo "[$(date)]: backup: Create $BKPDIR directory" >>$LOGFILE
@@ -112,47 +117,59 @@ if [ ! -d $NFSDIR ]
 then
         mkdir -p $NFSDIR 1>/dev/null 2>>$LOGFILE
         LOG $? 4 $NFSDIR
+    echo "    make $NFSDIR directory    "
         echo "[$(date)]: backup: Create $NFSDIR directory" >>$LOGFILE
 fi
 cut -d: -f3 /etc/passwd | grep '^505$' 1>/dev/null 2>&1
 if [ "$?" -ne 0 ]
 then
+    echo "    Adding  backup user in system    "
     useradd -u 505 backup && echo "[$(date)]: Adding user $(id backup)" >>$LOGFILE
     LOG $? 4 "user backup(uid=505)"
 fi
+echo "    Cleanning old backup files    "
 CleanUp 1
 echo "[$(date)]: Initialization successfully" >>$LOGFILE
+echo "----Initialization successfully----"
 
 #Starting BackupData function
 echo "[$(date)]: Beginning backup data" >>$LOGFILE
+echo "----Beginning backup data----"
 BackupData
 echo "[$(date)]: Backup successfully" >>$LOGFILE
+echo "----Backup successfully----"
 
 #Starting camNFS function
+echo "----Mount NFS----"
 echo "[$(date)]: Mount NFS directory" >>$LOGFILE
 camNFS
 echo "[$(date)]: Mount successfully" >>$LOGFILE
-
+echo "----Mount successfully----"
 #Host creatting its own backup directory on NFS directory
 if [ ! -d $NFSDIR/$HOST ]
 then
+    echo "Creating own backup directory in NFS"
     sudo -u#505 mkdir $NFSDIR/$HOST 1>/dev/null 2>>$LOGFILE
     LOG $? 4 "$NFSDIR/$HOST"
+    echo "Create done"
 fi
 echo "$NFSDIR/$HOST is existing" >>$LOGFILE
 
 #Clean the old data from NFS directory
+echo "----Clean old backup file in NFS----"
 echo "[$(date)]: Clean the Old backup" >>$LOGFILE
 sudo -u#505 rm -fr $NFSDIR/$HOST/*
 echo "[$(date)]: Clean Up" >>$LOGFILE
+echo "----Clean up----"
 
 #Starting backup transmission
+echo "----Starting transist----"
 echo "[$(date)]: Transisting" >>$LOGFILE
 sudo -u#505 cp -a $BKPDIR* $NFSDIR/$HOST/
 LOG $? 3 
 echo "[$(date)]: Transist successfully" >>$LOGFILE
 echo "[$(date)]: Backup data had been copyed (successfully!!!!)" >>$LOGFILE
-
+echo "----Transist successfully----"
 #if you couldn't see the logfile in the NFS directory affer script done that means the backup is unsuccessfully. 
 #You must cheack the log from Host.
 /bin/chmod 777 $LOGFILE
